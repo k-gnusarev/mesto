11247284ec79649af0c89a857fd4eb02bdbfe598
Konstantin Gnusarev <gnusarevkv@gmail.com>
2020-09-12 16:17:30 -0600

    реализовать функцию добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,28 @@
-const popup = document.querySelector('.popup');
-const popupHeader = document.querySelector('.popup__title');
+// ссылки на элементы формы редактирования профиля
+const editPopup = document.querySelector('.popup_type_edit');
 const editButton = document.querySelector('.profile__edit-button');
-const addButton = document.querySelector('.profile__add-button');
-const likeButton = document.querySelector('.card__like-button');
-const closeButton = popup.querySelector('.popup__close-button');
-const submitButton = popup.querySelector('.popup__submit-button');
-const editForm = document.querySelector('.popup__form');
+const editForm = document.querySelector('.popup__form_type_edit');
+const editPopupCloseButton = editPopup.querySelector('.popup__close-button');
 
-// переменные для полей попапа
-let popupTitleField = popup.querySelector('input[name="title"]');
-let popupSubtitleField = popup.querySelector('input[name="subtitle"]');
+// ссылки на элементы формы добавления карточки
+const addPopup = document.querySelector('.popup_type_add');
+const addButton = document.querySelector('.profile__add-button');
+const addForm = document.querySelector('.popup__form_type_add');
+const addPopupCloseButton = addPopup.querySelector('.popup__close-button');
+
+const likeButton = document.querySelector('.card__like-button');
+
+// переменные для полей попапа редактирования профиля
+const popupTitleField = editPopup.querySelector('input[name="title"]');
+const popupSubtitleField = editPopup.querySelector('input[name="subtitle"]');
 
 // переменные для текущих имени и описания профиля
-let currentProfileTitle = document.querySelector('.profile__title');
-let currentProfileSubtitle = document.querySelector('.profile__subtitle');
+const currentProfileTitle = document.querySelector('.profile__title');
+const currentProfileSubtitle = document.querySelector('.profile__subtitle');
 
-// объект, в котором хранятся свойства попапа
-const popupProperties = {
-  headingText: '',
-  titleFieldPlaceholder: '',
-  subtitleFieldPlaceholder: '',
-  submitText: ''
-}
+// переменные для полей попапа добавления места
+const newPlaceTitleField = addPopup.querySelector('input[name="placeName"]');
+const newLinkSubtitleField = addPopup.querySelector('input[name="placeLink"]');
 
 // изначальные карточки
 
@@ -29,137 +30,123 @@ const initialCards = [
   {
     name: 'Семук-Чампей',
     link: 'https://images.unsplash.com/photo-1525454240972-e37288888ff0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
-    altText: 'На фото: вид с высоты на долину реки'
   },
   {
     name: 'Антигуа-Гватемала',
     link: 'https://images.unsplash.com/photo-1563442744-3e17a3bf4932?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE0MjB9&auto=format&fit=crop&w=1950&q=80',
-    altText: 'На фото: колониальный город с видом на вулкан'
   },
   {
     name: 'Вулкан Фуэго',
     link: 'https://images.unsplash.com/photo-1506467493604-25d7861a6703?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
-    altText: 'На фото: вид на извергающийся вулкан'
   },
   {
     name: 'Тикаль',
     link: 'https://images.unsplash.com/photo-1508035460735-91088c495500?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
-    altText: 'На фото: руины индейских пирамид'
   },
   {
     name: 'Озеро Атитлан',
     link: 'https://images.unsplash.com/photo-1528543010705-e7e75169b717?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
-    altText: 'На фото: вид на озеро с вулканом на фоне'
   },
   {
     name: 'Остров Флорес',
     link: 'https://images.unsplash.com/photo-1544527232-c8738c8cb2cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
-    altText: 'На фото: вид на город с воды'
   }
 ];
 
-// прогрузить начальные карточки
-initialCards.forEach((place) => {
-  // скопировать новый элемент из темплейта
-  const cardTemplate = document.querySelector('.card-template').content;
-  const cardItem = cardTemplate.cloneNode(true);
-
-  // вписать соответствующие данные карточки
-  cardItem.querySelector('.card__title').textContent = place.name;
-  cardItem.querySelector('.card__photo').setAttribute('src', place.link);  
-  cardItem.querySelector('.card__photo').setAttribute('alt', place.altText);
-  
-  // like
-  cardItem.querySelector('.card__like-button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('card__like-button_active');
-  });
-
-  // разместить карточки
-  document.querySelector('.content').append(cardItem);
 
 
-});
-
-// закрытие окна по окончанию анимации
-const popupClose = () => {  
-  popup.classList.toggle('popup_state_closed');
-  popup.classList.toggle('popup_state_opened');
-  popup.removeAttribute('style');
-  popup.removeEventListener('animationend', popupClose);
-  popupTitleField.value = '';  
-  popupSubtitleField.value = '';
-} 
-
-// выключатель окна редактирования профиля
-const togglePopup = () => {
-  if (popup.classList.value.includes('popup_state_closed')) {
-    popup.classList.toggle('popup_state_opened');
-    popup.classList.toggle('popup_state_closed');
-  } else {
-    // подождать окончания анимации, затем убрать попап
-    popup.style.animation = 'hidePopup .2s linear forwards';
-    popup.addEventListener('animationend', popupClose);
+// выключатель попапа
+const togglePopup = (target) => {
+  if
+  ((target === editButton && !editPopup.classList.value.includes('popup_active')) ||
+  (target === editPopupCloseButton && editPopup.classList.value.includes('popup_active'))  
+  ) {
+    editPopup.classList.toggle('popup_active');
   }
-
-  // задать свойства полей, соответствующие функции попапа
-  popupHeader.textContent = popupProperties.headingText;
-  popupTitleField.setAttribute('placeholder', popupProperties.titleFieldPlaceholder);
-  popupSubtitleField.setAttribute('placeholder', popupProperties.subtitleFieldPlaceholder);
-  submitButton.textContent = popupProperties.submitText;
+  else if
+  ((target === addButton && !addPopup.classList.value.includes('popup_active')) ||
+  (target === addPopupCloseButton && addPopup.classList.value.includes('popup_active'))   
+  ) {
+    addPopup.classList.toggle('popup_active');
+  }
 }
 
-
 // вызвать окно редактирования профиля
-const editButtonPressed = function () {
-  popupProperties.headingText = 'Редактировать профиль';  
-  popupProperties.titleFieldPlaceholder = 'Имя профиля';  
-  popupProperties.subtitleFieldPlaceholder = 'Описание профиля';
-  popupProperties.submitText = 'Сохранить';
-
-  togglePopup();
-
+const toggleEditPopup = (evt) => {
+  togglePopup(evt.target);
   // вынести имя и описание профиля по умолчанию в текстовые поля попапа
   popupTitleField.value = currentProfileTitle.innerText;  
   popupSubtitleField.value = currentProfileSubtitle.innerText;
 }
 
-// вызвать окно добавления места
-const addButtonPressed = function () {  
-  popupProperties.headingText = 'Новое место';  
-  popupProperties.titleFieldPlaceholder = 'Название';  
-  popupProperties.subtitleFieldPlaceholder = 'Ссылка на картинку';
-  popupProperties.submitText = 'Создать';
-
-  togglePopup();
-}
-
-// сохранить изменения профиля и закрыть попап
-const formSubmit = function (event) {
-  event.preventDefault();
-
+// передать изменения информации профиля
+const saveProfileChanges = () => {
   currentProfileTitle.textContent = popupTitleField.value;
   currentProfileSubtitle.textContent = popupSubtitleField.value;
+}
+
+// вызвать окно добавления места
+const toggleAddPopup = (evt) => {
+  togglePopup(evt.target);
+}
+
+// разместить новую карточку
+
+const postNewCard = (place, link) => {
+  const cardTemplate = document.querySelector('.card-template').content;
+  const cardItem = cardTemplate.cloneNode(true);
+
+  cardItem.querySelector('.card__title').textContent = place;
+  cardItem.querySelector('.card__photo').setAttribute('src', link);
+
+  // установить кнопку лайка
+  cardItem.querySelector('.card__like-button').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('card__like-button_active');
+  });
+
+  document.querySelector('.content').prepend(cardItem);
+}
+
+// прогрузить начальные карточки
+initialCards.forEach(defaultCard => postNewCard(defaultCard.name, defaultCard.link));
+
+// добавить новую карточку
+const submitAddForm = (evt) => {
+  evt.preventDefault();
+  const newPlace = newPlaceTitleField.value;
+  const newLink = newLinkSubtitleField.value;
+
+  postNewCard(newPlace, newLink);
   
-  togglePopup();
+  addPopup.classList.toggle('popup_active');
+
+  newPlaceTitleField.value = '';
+  newLinkSubtitleField.value = '';
+};
+
+// сохранить изменения профиля и закрыть попап
+const submitEditForm = (event) => {
+  event.preventDefault();
+  saveProfileChanges();  
+  editPopup.classList.toggle('popup_active');
 }
 
-// закрыть форму по щелчку по затемнённой области
-const popupCloseByClickOnOverlay = (event) => {
-  if (event.target != event.currentTarget) {
-    return;
+const popupCloseByClickOnOverlay = (evt) => {
+  if (evt.target === editPopup) {
+    editPopup.classList.toggle('popup_active');
+  } else if (evt.target === addPopup) {
+    addPopup.classList.toggle('popup_active');
   }
-  togglePopup();
 }
 
-// поставить/убрать лайк
+// обработчики формы редактирования профиля
+editButton.addEventListener('click', toggleEditPopup);
+editPopupCloseButton.addEventListener('click', toggleEditPopup);
+editPopup.addEventListener('click', popupCloseByClickOnOverlay);
+editForm.addEventListener('submit', submitEditForm);
 
-const toggleLike = function () {
-  likeButton.classList.toggle('card__like-button_active');
-}
-
-//likeButton.addEventListener('click', toggleLike);
-editButton.addEventListener('click', editButtonPressed);
-addButton.addEventListener('click', addButtonPressed);
-closeButton.addEventListener('click', togglePopup);
-editForm.addEventListener('submit', formSubmit);
-popup.addEventListener('click', popupCloseByClickOnOverlay);
+// обработчики формы добавления карточки
+addButton.addEventListener('click', toggleAddPopup);
+addPopupCloseButton.addEventListener('click', toggleAddPopup);
+addPopup.addEventListener('click', popupCloseByClickOnOverlay);
+addForm.addEventListener('submit', submitAddForm);
