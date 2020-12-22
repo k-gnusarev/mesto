4d60528107aff32ce4efcd37645ff069fe674b00
k-gnusarev <kgnusaryov@yandex.ru>
2020-12-22 16:57:24 -0600

    feat: настройка gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,18 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[contenthash][ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[contenthash][ext]'
+        }
       },
       {
         test: /\.css$/,
